8551d37b5209ff6abd77b391d378396c1d75b8bf
Eugenio Cano-Manuel Mendoza <eugeniocanom@gmail.com>
2020-02-15 17:09:04 +0000

    Taking lcd stuff out

---
+++ lcd.h
@@ -0,0 +1,13 @@
+void lcd_init(void);
+void lcd_send_command(int, int, char);
+void lcd_clear(void);
+void lcd_return_home(void);
+void lcd_move_first_line(void);
+void lcd_move_second_line(void);
+void lcd_print_char(char);
+void lcd_print_byte(char);
+void lcd_print_text(const char*);
+void lcd_print_text2(const char*, int);
+void lcd_show(const char*, const char*);
+void lcd_show2(const char*);
+void lcd_clear_and_home(void);
